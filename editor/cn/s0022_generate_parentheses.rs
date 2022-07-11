27ccf7a0d1d16b7583dd09e0c2890b3e8a6f073e
w01fb0ss <leen.zhou.cn@gmail.com>
2022-07-11 18:25:16 +0800

    括号生成

--- editor/cn/s0022_generate_parentheses.rs
+++ editor/cn/s0022_generate_parentheses.rs
@@ -3,7 +3,25 @@ struct Solution;
 //leetcode submit region begin(Prohibit modification and deletion)
 impl Solution {
     pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut ret = Vec::new();
+        Self::recur(String::new(), n, n, &mut ret);
+        ret
+    }
 
+    fn recur(s: String, left: i32, right: i32, ret: &mut Vec<String>) {
+        if left > right{
+            return;
+        }
+        if right == 0 {
+            ret.push(s);
+            return;
+        }
+        if left > 0 {
+            Self::recur(format!("{}{}", s, '('), left - 1, right, ret);
+        }
+        if right > 0 {
+            Self::recur(format!("{}{}", s, ')'), left, right - 1, ret);
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
