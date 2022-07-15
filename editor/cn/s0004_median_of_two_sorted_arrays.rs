67dc0f1ad53f9193916996c18792099c35586231
w01fb0ss <leen.zhou.cn@gmail.com>
2022-07-15 18:17:56 +0800

    leetcode

---
+++ editor/cn/s0004_median_of_two_sorted_arrays.rs
@@ -0,0 +1,21 @@
+struct Solution;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // use rustgym_util::*;
+
+    #[test]
+    fn test() {
+        
+    }
+}
---
+++ editor/cn/s面试题 02.06_palindrome_linked_list_lcci.rs
@@ -0,0 +1,37 @@
+struct Solution;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // use rustgym_util::*;
+
+    #[test]
+    fn test() {
+        
+    }
+}
